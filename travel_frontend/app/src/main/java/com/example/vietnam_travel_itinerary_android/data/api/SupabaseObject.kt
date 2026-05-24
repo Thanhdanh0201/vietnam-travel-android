509e5fe0db58f92d62ded8c54caf674e7425b82d
phamthinh2511 <flare.dsly@gmail.com>
2026-05-24 11:22:06 +0700

    Feat: Community-Implement

--- travel_frontend/app/src/main/java/com/example/vietnam_travel_itinerary_android/data/api/SupabaseObject.kt
+++ travel_frontend/app/src/main/java/com/example/vietnam_travel_itinerary_android/data/api/SupabaseObject.kt
@@ -3,13 +3,19 @@ package com.example.vietnam_travel_itinerary_android
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.realtime.Realtime
+import io.github.jan.supabase.storage.Storage
 
 object SupabaseObject {
     val client: SupabaseClient = createSupabaseClient(
         supabaseUrl = "https://jfaxmpasqkzztnvckqjl.supabase.co",
         supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmYXhtcGFzcWt6enRudmNrcWpsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzMjIxMTgsImV4cCI6MjA4OTg5ODExOH0.ZBD2zc-u1mjFDdqaMHKHBiiXBtJBydNBSPAS4Y_XzSU"
     ) {
-        // Install Auth module
+        // Install modules
         install(Auth)
+        install(Postgrest)
+        install(Storage)
+        install(Realtime)
     }
 }
